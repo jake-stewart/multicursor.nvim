f1f7383990be20cdca7a35fc23d34fbd55aaa0dd
Jake Stewart <jake.stephen.stewart@gmail.com>
2024-09-15 10:51:55 +0800

    update cursor pos after cmd

--- lua/multicursor-nvim/input-manager.lua
+++ lua/multicursor-nvim/input-manager.lua
@@ -96,6 +96,11 @@ function InputManager:_onSafeState()
         if self._cmdType == ":" then
             self._cmdType = nil
             self._macro = ""
+            self._cursorManager:action(function(ctx)
+                ctx:forEachCursor(function(cursor)
+                    cursor:feedkeys("")
+                end)
+            end)
             return
         end
         self._cmdType = nil
