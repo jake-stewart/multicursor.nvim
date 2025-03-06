156fea1add23a45e3326e3dc3f2dd14d433c899e
Jake Stewart <jake.stephen.stewart@gmail.com>
2025-03-06 10:04:31 +0800

    tidy termcodes

--- lua/multicursor-nvim/term-codes.lua
+++ lua/multicursor-nvim/term-codes.lua
@@ -1,16 +1,20 @@
+local function termcode(key)
+    return vim.api.nvim_replace_termcodes(key, true, true, true)
+end
+
 return {
-    ESC = vim.api.nvim_replace_termcodes("<esc>", true, true, true),
-    BACKSPACE = vim.api.nvim_replace_termcodes("<bs>", true, true, true),
-    CTRL_A = vim.api.nvim_replace_termcodes("<c-a>", true, true, true),
-    CTRL_X = vim.api.nvim_replace_termcodes("<c-x>", true, true, true),
-    CTRL_V = vim.api.nvim_replace_termcodes("<c-v>", true, true, true),
-    CTRL_S = vim.api.nvim_replace_termcodes("<c-s>", true, true, true),
-    CTRL_R = vim.api.nvim_replace_termcodes("<c-r>", true, true, true),
-    CTRL_G = vim.api.nvim_replace_termcodes("<c-g>", true, true, true),
-    CTRL_E = vim.api.nvim_replace_termcodes("<c-e>", true, true, true),
-    CTRL_Y = vim.api.nvim_replace_termcodes("<c-y>", true, true, true),
-    CTRL_I = vim.api.nvim_replace_termcodes("<c-i>", true, true, true),
-    CTRL_O = vim.api.nvim_replace_termcodes("<c-o>", true, true, true),
-    LEFT = vim.api.nvim_replace_termcodes("<left>", true, true, true),
-    RIGHT = vim.api.nvim_replace_termcodes("<right>", true, true, true)
+    ESC = termcode("<esc>"),
+    BACKSPACE = termcode("<bs>"),
+    CTRL_A = termcode("<c-a>"),
+    CTRL_X = termcode("<c-x>"),
+    CTRL_V = termcode("<c-v>"),
+    CTRL_S = termcode("<c-s>"),
+    CTRL_R = termcode("<c-r>"),
+    CTRL_G = termcode("<c-g>"),
+    CTRL_E = termcode("<c-e>"),
+    CTRL_Y = termcode("<c-y>"),
+    CTRL_I = termcode("<c-i>"),
+    CTRL_O = termcode("<c-o>"),
+    LEFT = termcode("<left>"),
+    RIGHT = termcode("<right>"),
 }
