482bdd635b282ec1cb27639f8950644482aa83a0
Jake Stewart <jake.stephen.stewart@gmail.com>
2025-12-22 14:55:23 +0800

    improve add cursor op

--- lua/multicursor-nvim/examples.lua
+++ lua/multicursor-nvim/examples.lua
@@ -838,9 +838,20 @@ end
 ---
 --- For example, if it is mapped to `ga`, then typing `gaip` will add a
 --- cursor for every line in the current paragraph.
-function examples.addCursorOperator()
+--- @param placement? "START_OF_LINE" | "START_OF_SELECTION" | "CURSOR"
+function examples.addCursorOperator(placement)
     local mode = vim.fn.mode()
+    if not placement then
+        if mode == TERM_CODES.CTRL_V or mode == TERM_CODES.CTRL_S then
+            placement = "START_OF_SELECTION"
+        elseif mode == "n" then
+            placement = "CURSOR"
+        else
+            placement = "START_OF_LINE"
+        end
+    end
     local curPos = vim.fn.getpos(".")
+    local vPos = vim.fn.getpos("v")
     local fromVisual = mode == "v"
         or mode == "V"
         or mode == TERM_CODES.CTRL_V
@@ -849,7 +860,6 @@ function examples.addCursorOperator()
         or mode == TERM_CODES.CTRL_S
     local atVisualStart
     if fromVisual then
-        local vPos = vim.fn.getpos("v")
         atVisualStart = curPos[2] < vPos[2]
             or curPos[2] == vPos[2]
             and (curPos[3] < vPos[3]
@@ -864,10 +874,19 @@ function examples.addCursorOperator()
             local changeStart = vim.fn.getpos("'[")
             local changeEnd = vim.fn.getpos("']")
             for i = changeStart[2], changeEnd[2] do
-                local col = math.min(
-                    curPos[3],
-                    vim.fn.col({ i, "$" }) - 1
-                )
+                local col = 1
+                if placement == "CURSOR" then
+                    col = curPos[3]
+                elseif placement == "START_OF_SELECTION" then
+                    if mode == TERM_CODES.CTRL_V or mode == TERM_CODES.CTRL_S then
+                        if atVisualStart then
+                            col = curPos[3]
+                        else
+                            col = vPos[3]
+                        end
+                    end
+                end
+                col = math.min(col, vim.fn.col({ i, "$" }) - 1)
                 lastCursor = mainCursor:clone():setPos({i, col})
                 if not firstCursor then
                     firstCursor = lastCursor
